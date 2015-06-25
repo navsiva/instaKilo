8870dbe5fb71a0daae4764f80186076e86559c0f
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-06-25 11:25:45 -0400

    Initial Commit

---
+++ instaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instaKilo
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-06-25.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instaKilo
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-06-25.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
